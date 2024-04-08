82a443af8921414a4552ecc53336a7f730cf0110
Kebin Wang <kebinwang@gmail.com>
2024-04-08 09:24:08 -0400

    Updates

---
+++ precache-manifest.048317ccf9cd7f2c1c1f96e785d008d9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "c11b94876fc3d49fbbf3",
+    "url": "/static/css/main.79198b37.chunk.css"
+  },
+  {
+    "revision": "c11b94876fc3d49fbbf3",
+    "url": "/static/js/main.d66c2e19.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "994ed2b109ca3847dbb7",
+    "url": "/static/css/2.556faddf.chunk.css"
+  },
+  {
+    "revision": "994ed2b109ca3847dbb7",
+    "url": "/static/js/2.0450a161.chunk.js"
+  },
+  {
+    "revision": "43d1d1db26878698569fa007578d8779",
+    "url": "/static/media/kb.43d1d1db.jpg"
+  },
+  {
+    "revision": "c4ba7c12a97cfcf5f6311e6e9057b6c6",
+    "url": "/static/media/Kebin_Resume.c4ba7c12.pdf"
+  },
+  {
+    "revision": "cd4b1396b837ec0dbccb60ff1acc9b47",
+    "url": "/static/media/background.cd4b1396.jpg"
+  },
+  {
+    "revision": "75486e9c8b090f89db126ebebb4179ff",
+    "url": "/static/media/bigback.75486e9c.jpg"
+  },
+  {
+    "revision": "67edcff8f089d9f2040bae3eb463d29e",
+    "url": "/static/media/demo.67edcff8.svg"
+  },
+  {
+    "revision": "42d7f548e5c0454b75915015d392e756",
+    "url": "/index.html"
+  }
+];
